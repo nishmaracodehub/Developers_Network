09da45db62054a997db18bdca5f71a1d1a30beea
nishmaracodehub <nishanthmaraboina@gmail.com>
2019-08-21 22:27:30 +0530

    Profile and Post APIs

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,89 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Models
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+
+//validation
+const validatePostInput = require("../../validation/post");
 
 // @route   GET api/posts/test
 // @desc    Tests posts route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+// @route   GET api/posts
+// @desc    GET posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No Posts Found" }));
+});
+
+// @route   GET api/post/:id
+// @desc    GET post by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No Post Found with that ID" })
+    );
+});
+
+// @route   POST api/posts
+// @desc    create post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //check validation
+    if (!isValid) {
+      return res.status(404).json(errors);
+    }
+
+    const { text, name, avatar } = req.body;
+    const newPost = new Post({
+      text,
+      name,
+      avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    create post by id
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        //check for post user
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ notauthorized: "User not authorized" });
+        }
+
+        //Delete
+        post
+          .remove()
+          .then(() => res.json({ success: true }))
+          .catch(() => res.status(404).json({ postnotfound: "No Post Found" }));
+      });
+    });
+  }
+);
 
 module.exports = router;
